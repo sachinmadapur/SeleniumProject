5ba0b45f5102c17378be045d32ee400d74595569
sachinmadapur <sachinmadapur8@gmail.com>
2025-03-17 22:10:41 +0530

    pushed

---
+++ com.zohocrm.in3/src/test/java/pac2/orgTest.java
@@ -0,0 +1,18 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+@Test
+public void createOrgtest() {
+	System.out.println("createContact");
+}
+@Test
+public void modifyOrgtest() {
+	System.out.println("modifyContact");
+}
+@Test
+public void NewOrgPageinfo() {
+	System.out.println("New Page Created");
+}
+}
